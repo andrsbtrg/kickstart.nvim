9955aa4b122deefcbf947ddea4c2d15c25aeb0cf
Andres <andrsbtrg@gmail.com>
2024-06-25 20:58:17 +0200

    Let vim default colors

--- init.lua
+++ init.lua
@@ -164,8 +164,8 @@ vim.opt.hlsearch = true
 function Transparent(color)
   color = color or 'default'
   vim.cmd.colorscheme(color)
-  vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
-  vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+  -- vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
 end
 Transparent()
 
