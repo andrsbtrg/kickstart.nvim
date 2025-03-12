9fcca924c1aac0e44eb971c02e0e82c7e5e0e4b7
Andres <andrsbtrg@gmail.com>
2025-03-12 21:25:39 +0100

    replace mini status line with lualine

--- init.lua
+++ init.lua
@@ -860,29 +860,28 @@ require('lazy').setup({
       -- - sr)'  - [S]urround [R]eplace [)] [']
       require('mini.surround').setup()
 
-      -- Simple and easy statusline.
-      --  You could remove this setup call if you don't like it,
-      --  and try some other statusline plugin
-      local statusline = require 'mini.statusline'
-      -- set use_icons to true if you have a Nerd Font
-      statusline.setup { use_icons = vim.g.have_nerd_font }
-
-      -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we set the section for
-      -- cursor location to LINE:COLUMN
-      ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function()
-        return '%2l:%-2v'
-      end
-
-      require('mini.files').setup()
-
-      -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
+      require('mini.files').setup {
+        options = {
+          permanently_delete = false,
+        },
+      }
     end,
     keys = {
       { '\\', ':lua MiniFiles.open()<CR>', silent = true, { desc = 'Files' } },
     },
+  },
+  {
+    'nvim-lualine/lualine.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      require('lualine').setup {
+        options = {
+          section_separators = '',
+          component_separators = '',
+          icons_enabled = false,
+        },
+      }
+    end,
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
@@ -963,4 +962,3 @@ require('lazy').setup({
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
-vim.cmd.colorscheme 'oldworld'
--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -2,4 +2,7 @@ return {
   'dgox16/oldworld.nvim',
   lazy = false,
   priority = 1000,
+  config = function()
+    vim.cmd.colorscheme 'oldworld'
+  end,
 }
