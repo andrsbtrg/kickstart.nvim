013d7c410c1fe007ffe6bcd6885883aa4d462379
Andres <andrsbtrg@gmail.com>
2025-09-06 21:10:53 +0200

    chore: clean init.lua and change colors

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -1,15 +1,10 @@
--- lua/plugins/rose-pine.lua
 return {
-  'rose-pine/neovim',
-  name = 'rose-pine',
-  config = function()
-    require('rose-pine').setup {
-      styles = {
-        bold = true,
-        italic = false,
-        transparency = true,
-      },
-    }
-    vim.cmd 'colorscheme rose-pine'
+  'slugbyte/lackluster.nvim',
+  lazy = false,
+  priority = 1000,
+  init = function()
+    vim.cmd.colorscheme 'lackluster'
+    -- vim.cmd.colorscheme("lackluster-hack") -- my favorite
+    -- vim.cmd.colorscheme("lackluster-mint")
   end,
 }
