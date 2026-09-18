42df334247697972fbea3d13112c532599389717
Andres <andrsbtrg@gmail.com>
2026-09-18 08:20:05 +0200

    feat: Change colorscheme with os theme

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -4,4 +4,16 @@ require('vague').setup {
   italic = false,
 }
 
-vim.cmd 'colorscheme default'
+local function make_transparent()
+  local groups = { 'Normal', 'NormalNC', 'NormalFloat', 'SignColumn', 'StatusLine', 'StatusLineNC', 'EndOfBuffer' }
+  for _, group in ipairs(groups) do
+    vim.api.nvim_set_hl(0, group, { bg = 'NONE', ctermbg = 'NONE' })
+  end
+end
+
+make_transparent()
+vim.api.nvim_create_autocmd('ColorScheme', {
+  callback = make_transparent,
+})
+
+vim.cmd 'colorscheme vague'
